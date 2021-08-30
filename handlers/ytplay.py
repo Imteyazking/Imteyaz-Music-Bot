839b21095555194e7f17af37eac33c2513237bed
Imteyaz_king <79402181+Imteyazking@users.noreply.github.com>
2021-08-30 23:44:05 +0530

    Update ytplay.py

--- handlers/ytplay.py
+++ handlers/ytplay.py
@@ -61,8 +61,8 @@ async def play(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="LOVE TALKS ⚔️ ",
-                        url=f"{https://t.me/love_talks_fam}")
+                        text="Nisha Music ❤️ ",
+                        url=f"{https://t.me/Nisha_Music_Support}")
                    
                 ]
             ]
@@ -72,8 +72,8 @@ async def play(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="LOVE TALKS ⚔️",
-                        url=f"{https://t.me/love_talks_fam}")
+                        text="Nisha Music ❤️",
+                        url=f"{https://t.me/Nisha_Music_Support}")
                    
                 ]
             ]
